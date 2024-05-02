6cf4208af4b7513ff9eb9572e8259db01693306c
unknown <wawerufelix60@gmail.com>
2024-05-02 08:41:12 +0300

    feat: demo updates

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/api/data", (req, res) => {
     const data = {
-      message: "This is a sample JSON response from the API."
+        message: "This is a sample JSON response from the API."
     };
     res.json(data);
-  });
+});
 
-  const PORT = 3000;
-  app.listen(PORT, () => {
-  console.log(`🎉 Server is live on port:${PORT} 🎉`);
+// New Route
+app.get("/api/users", (req, res) => {
+    const users = [
+        { id: 1, name: "Alice" },
+        { id: 2, name: "Bob" }
+    ];
+    res.json(users);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`🎉 Server is live on port:${PORT} 🎉`);
 });
